9a31d404fcfd9490c611e81cd9878a7aa3cb6651
Petr Tarasenko <pereter92@gmail.com>
2017-08-01 17:50:47 +0300

    deleted out folder

--- out/_next/a973bea8-2b88-4605-9d6b-1a212ab88771/page/about/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/about', function() {
-            var comp = module.exports=webpackJsonp([6],{345:function(e,t,u){e.exports=u(346)},346:function(e,t,u){"use strict";function r(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var n=u(6),o=r(n),a=u(170),s=(r(a),"/Users/tarasenko/projects/hello-next/pages/about.js?entry");t.default=function(){return o.default.createElement("div",{__source:{fileName:s,lineNumber:4}},o.default.createElement("p",{__source:{fileName:s,lineNumber:6}},"This is the about page"))}}},[345]);
-            return { page: comp.default }
-          })
-        
